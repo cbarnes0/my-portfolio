8f186f08c29c2b318999136366cbe863bc591d09
Carson <carsonbarnes98@gmail.com>
2023-05-31 14:41:56 -0400

    error with dropdown menu fixed

--- src/component/AboutMe.jsx
+++ src/component/AboutMe.jsx
@@ -26,11 +26,11 @@ const AboutMe = () => {
             team that values innovation, learning, and collaboration.
             </p>
             <a href="#Contact"
-	        class="group relative inline-block overflow-hidden rounded border border-gray-100 bg-gray-200  px-12 py-3 text-2xl text-center font-[Monospace] text-slate-800 hover:text-teal-600 focus:outline-none focus:ring active:bg-indigo-600 active:text-white">
-	            <span class="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-teal-600 transition-all duration-200 group-hover:w-full"></span>
-	            <span class="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-teal-600 transition-all duration-200 group-hover:h-full"></span>
-	            <span class="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-teal-600 transition-all duration-200 group-hover:w-full"></span>
-	            <span class="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-teal-600 transition-all duration-200 group-hover:h-full"></span>
+	        className="group relative inline-block overflow-hidden rounded border border-gray-100 bg-gray-200  px-12 py-3 text-2xl text-center font-[Monospace] text-slate-800 hover:text-teal-600 focus:outline-none focus:ring active:bg-indigo-600 active:text-white">
+	            <span className="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-teal-600 transition-all duration-200 group-hover:w-full"></span>
+	            <span className="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-teal-600 transition-all duration-200 group-hover:h-full"></span>
+	            <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-teal-600 transition-all duration-200 group-hover:w-full"></span>
+	            <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-teal-600 transition-all duration-200 group-hover:h-full"></span>
 	            Contact Me
             </a>
           </div>
